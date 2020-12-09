91427b988924eb722ffe6d4b91daa7211bdd4a2d
zhushuai74 <2317709047@qq.com>
2020-12-09 10:58:15 +0800

    python

--- test-DESKTOP-BDG32JS.py
+++
@@ -1,8 +0,0 @@
-import requests
-headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36 Edg/83.0.478.54'
-}
-response = requests.get('http://www.ofzhus.cn',headers)
-print(response.text)
-with open('boke','wb') as f:
-        f.write(response.content)
